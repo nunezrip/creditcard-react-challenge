4a7556bac4d9f0e9d81a25833236433e4bcaf51b
nunezrip <nunezrip@aol.com>
2018-03-05 20:26:11 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CreditCard from './components/CreditCard/CreditCard';
+
+ReactDOM.render(<CreditCard />, document.getElementById('root'));
